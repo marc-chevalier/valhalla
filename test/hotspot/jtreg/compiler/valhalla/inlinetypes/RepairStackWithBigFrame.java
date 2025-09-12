673e29ab3222e9a2f6b505bd69a4fb658df1d898
Marc Chevalier <marc.chevalier@oracle.com>
2025-09-12 12:15:24 +0000

    simplify

--- test/hotspot/jtreg/compiler/valhalla/inlinetypes/RepairStackWithBigFrame.java
+++ test/hotspot/jtreg/compiler/valhalla/inlinetypes/RepairStackWithBigFrame.java
@@ -23,73 +23,38 @@
 
 package compiler.valhalla.inlinetypes;
 
-import jdk.internal.vm.annotation.NullRestricted;
-import jdk.internal.vm.annotation.Strict;
-import jdk.test.lib.Asserts;
-
 import static compiler.valhalla.inlinetypes.InlineTypes.*;
 
 /*
  * @test
  * @bug 8367156
- * @summary On Aarch64, when the frame is too big and we need to repair it after
+ * @summary On Aarch64, when the frame is very big and we need to repair it after
  *          scalarization of the arguments, we cannot use ldp to get the stack
- *          increment and rfp as is only has a 7 bit offset. Use two ldr with 9-bit
- *          offsets instead.
- * @library /test/lib /test/jdk/java/lang/invoke/common /
+ *          increment and rfp at the same time, since it only has a 7 bit offset.
+ *          We use two ldr with 9-bit offsets instead.
+ * @library /test/lib /
  * @requires (os.simpleArch == "x64" | os.simpleArch == "aarch64")
  * @enablePreview
- * @modules java.base/jdk.internal.value
- *          java.base/jdk.internal.vm.annotation
- *          java.base/jdk.internal.misc
+ * @modules java.base/jdk.internal.vm.annotation
  * @run main/othervm
  *      -Xcomp
- *      -XX:CompileCommand=compileonly,compiler.valhalla.inlinetypes.RepairStackWithBigFrame::test2
- *      -XX:+IgnoreUnrecognizedVMOptions
- *      -XX:-UseACmpProfile
- *      -XX:+AlwaysIncrementalInline
- *      -XX:FlatArrayElementMaxOops=5
- *      -XX:+UseArrayFlattening
- *      -XX:-UseArrayLoadStoreProfile
- *      -XX:+UseFieldFlattening
- *      -XX:+InlineTypePassFieldsAsArgs
- *      -XX:+InlineTypeReturnedAsFields
+ *      -XX:CompileCommand=compileonly,compiler.valhalla.inlinetypes.RepairStackWithBigFrame::test
  *      compiler.valhalla.inlinetypes.RepairStackWithBigFrame
  */
 
 public class RepairStackWithBigFrame {
+    static final MyValue1 testValue1 = MyValue1.createWithFieldsInline(rI, rL);
+
     public static void main(String[] args) {
-        new RepairStackWithBigFrame().test1();
+        new RepairStackWithBigFrame().test(testValue1);
     }
 
-    static {
-        // Make sure RuntimeException is loaded to prevent uncommon traps in IR verified tests
-        // RuntimeException tmp = new RuntimeException("42");
-        // NullPointerException _1 = new NullPointerException("");
-    }
-
-    // Helper methods
-
-    @Strict
-    @NullRestricted
-    private static final MyValue1 testValue1 = MyValue1.createWithFieldsInline(rI, rL);
-
-    @Strict
-    @NullRestricted
-    MyValue1 valueField1 = testValue1;
-
-
-    public long test2(MyValue1 arg) {
+    long test(MyValue1 arg) {
         MyAbstract vt1 = MyValue1.createWithFieldsInline(rI, rL);
         MyAbstract vt2 = MyValue1.createWithFieldsDontInline(rI, rL);
-        MyAbstract vt3 = arg;
-        MyAbstract vt4 = valueField1;
+        MyAbstract vt3 = MyValue1.createWithFieldsInline(rI, rL);
+        MyAbstract vt4 = arg;
         return ((MyValue1)vt1).hash() + ((MyValue1)vt2).hash() +
                ((MyValue1)vt3).hash() + ((MyValue1)vt4).hash();
-    }
-
-    public void test1() {
-        long result = test2(valueField1);
-        Asserts.assertEQ(result, 4*testValue1.hash());
     }
 }
