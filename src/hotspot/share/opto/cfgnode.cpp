939f5edc62bd7e0db4d873eb761a4b3197b2aa15
Marc Chevalier <marc.chevalier@oracle.com>
2025-09-16 16:11:20 +0200

    For testing

--- src/hotspot/share/opto/cfgnode.cpp
+++ src/hotspot/share/opto/cfgnode.cpp
@@ -1472,13 +1472,24 @@ Node* PhiNode::Identity(PhaseGVN* phase) {
   // It would check for a tributary phi on the backedge that the main phi
   // trivially, perhaps with a single cast.  The unique_input method
   // does all this and more, by reducing such tributaries to 'this'.)
-  Node* uin = unique_input(phase, false);
-  if (uin != nullptr) {
-    return uin;
+  {
+    Node* uin = unique_input(phase, false);
+    if (uin != nullptr) {
+      return uin;
+    }
   }
-  uin = unique_input_recursive(phase);
-  if (uin != nullptr) {
-    return uin;
+  {
+    Node* uin = unique_input_recursive(phase);
+    if (uin != nullptr) {
+      if (!uin->is_dead_loop_safe()) {
+        for (DUIterator_Fast imax, i = fast_outs(imax); i < imax; i++) {
+          if (fast_out(i) == uin) {
+            return phase->C->top();
+          }
+        }
+      }
+      return uin;
+    }
   }
 
   int true_path = is_diamond_phi();
